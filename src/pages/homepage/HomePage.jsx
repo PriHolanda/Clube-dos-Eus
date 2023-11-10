f7b2153c6e7462b48c27b0378376c84f9660b0ba
DimitryAlves <dimitryalvessilva@gmail.com>
2023-11-10 11:04:30 -0300

    finalização

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -5,14 +5,13 @@ import clipboard from '../../assets/img/clipboard.png'
 import materials from '../../assets/img/img-materials.png'
 import cards from '../../assets/img/img-material-cards.png'
 import portrait from '../../assets/img/img-material-portrait.png'
+import game from '../../assets/img/img-material-game.png'
 import book from '../../assets/img/img-material-book.png'
 import box from '../../assets/img/img-material-box.png'
 import megaphone from '../../assets/img/megaphone.png'
 import rael from '../../assets/img/img-rael.png'
 import julia from '../../assets/img/img-julia.png'
-import marilia from '../../assets/img/img-marilia.png'
 import priscila from '../../assets/img/img-priscila.png'
-import renato from '../../assets/img/img-renato.png'
 import joyce from '../../assets/img/img-joyce.png'
 import randson from '../../assets/img/img-randson.png'
 import dimitry from '../../assets/img/img-dimitry.png'
@@ -30,8 +29,9 @@ function HomePage() {
             <div className='showcase'>
                 <img src={cabecalho} alt="Imagem cabecalho" className='img-showcase' />
                 <div className="showcase-text">
-                    Construção da identidade racial: o processo de aceitação da criança negra e as lacunas educacionais geradas na primeira infância
+                    EDUCAÇÃO RACIAL NA IFÂNCIA: A VIRADA DE CHAVE PARA CONSCIÊNCIA RACIAL NA ADOLESCÊNCIA
                 </div>
+
             </div>
 
             {/* INTRODUÇÃO     */}
@@ -40,7 +40,9 @@ function HomePage() {
                 <img src={creators} alt="Criadores" className='img-project-creators' />
 
                 <div className='introduction-container'>
-                    <div className='introduction-block'> O que é o clube dos Eu's? </div>
+                    <div className='introduction-block'>
+                        O que é o clube dos Eu's?
+                    </div>
                     O Clube dos Eu’s, é muito mais que um projeto, é uma tentativa de mudança na sociedade. De maneira análoga ao pensamento de Leon Tolstói, no qual ele diz: “Se queres ser universal, começa por pintar a tua aldeia.” Acreditamos que uma mudança deva começar na nossa localidade. Então começamos, com ações antirracistas, e na base da educação, na educação   infantil. Com o auxílio da comunidade escolar, realizamos atividades interventivas nas quais propiciamos uma reflexão as crianças sobre sua própria identidade visual, e sua multiplicidade de identidades, realizamos uma pesquisa com as crianças que estão na primeira infância(0 a 6 anos) e constatamos a lacuna que foi gerada nesse processo. É notório a dificuldade de crianças, negras/pardas se identificarem como são. Por outro lado, os professores são nossos parceiros no processo, então para auxiliá-los, construímos um ebook com inúmeras formas de se trabalhar a temática na educação, além de construirmos materiais pedagógicos, que também se unem no processo de educação étnico-racial nessa faixa-etária.
                 </div>
             </div>
@@ -75,8 +77,15 @@ function HomePage() {
 
             <div className="tools">
                 <div className="tools-container">
-                    <div className="tools-container-kit">MATERIAIS PEDAGÓGICOS</div>
-                    <img src={clipboard} alt="Prancheta" className='tools-img-clipboard' />
+
+                    <div className="tools-container-kit">
+                        <h3>
+                            MATERIAIS PEDAGÓGICOS
+                        </h3>
+
+                        <img src={clipboard} alt="Prancheta" className='tools-img-clipboard' />
+                    </div>
+
                 </div>
 
                 <div className="tools-gallery">
@@ -86,6 +95,7 @@ function HomePage() {
                             <img src={book} alt="Livro de Colorir" className='item' />
                             <img src={cards} alt="Cards com personagens" className='item' />
                             <img src={box} alt="Caixa do Tesouro" className='item' />
+                            <img src={game} alt="Caixa do Tesouro" className='item' />
 
                         </div>
                     </div>
@@ -95,16 +105,21 @@ function HomePage() {
             {/* DOWNLOAD */}
 
             <div className="download-button-container">
-                <button className="button-download"><a href='https://projetoclube.files.wordpress.com/2022/11/clube-dos-eus-ebook.pdf' className='download-ebook'>DOWNLOAD EBOOK</a></button>
+                <button className="button-download">
+                    <a href='https://projetoclube.files.wordpress.com/2022/11/clube-dos-eus-ebook.pdf' className='download-ebook'>DOWNLOAD EBOOK</a>
+                </button>
+
+                <img src={megaphone} alt="Megafone" className='img-megaphone' />
 
             </div>
-            <img src={megaphone} alt="Megafone" className='img-megaphone' />
 
             {/* JOGO DA MEMORIA  */}
 
-           
+
             <div className="memory-game">
-                <img src={iphone} alt="Jogo da Memoria" className='img-memory-game' />
+                <div className='memory-game-img'>
+                    <img src={iphone} alt="Jogo da Memoria" className='img-memory-game' />
+                </div>
 
                 <div className='memory-game-container'>
                     <div className='memory-game-block'> JOGO DA MEMÓRIA ON-LINE </div>
@@ -113,11 +128,14 @@ function HomePage() {
             </div>
 
             <div className="link">
-                <button className="button-memory-game"><a href='https://jogo-clube-dos-eus.netlify.app/' className='link-memory-game'>JOGUE AQUI</a></button>
+                <button >
+                    <a href='https://jogo-clube-dos-eus.netlify.app/' className='link-memory-game'>
+                        JOGUE AQUI
+                    </a>
 
-                <img src={alvo} alt="Alvo" className='img-alvo' />
+                </button>
 
-            
+                <img src={alvo} alt="Alvo" />
             </div>
 
 
@@ -126,14 +144,25 @@ function HomePage() {
             <div className="creators-gallery">
                 <div className="creators-gallery-wrapper">
                     <div className="creators-gallery-container">
-                        <img src={rael} alt="rael" className='creator' />
-                        <img src={julia} alt="julia" className='creator' />
-                        <img src={marilia} alt="marilia" className='creator' />
-                        <img src={priscila} alt="priscila" className='creator' />
-                        <img src={renato} alt="renato" className='creator' />
-                        <img src={joyce} alt="joyce" className='creator' />
-                        <img src={randson} alt="randson" className='creator' />
-                        <img src={dimitry} alt="dimitry" className='creator' />
+                        <a href="https://www.instagram.com/rae1zin/">
+                            <img src={rael} alt="rael" className='creator' />
+                        </a>
+                        <a href="https://www.instagram.com/_julialsouza/">
+                            <img src={julia} alt="julia" className='creator' />
+                        </a>
+                        <a href="https://www.instagram.com/pri_hollanda/">
+                            <img src={priscila} alt="priscila" className='creator' />
+                        </a>
+                        <a href="https://www.instagram.com/joyce_geosales/">
+                            <img src={joyce} alt="joyce" className='creator' />
+                        </a>
+                        <a href="	https://www.linkedin.com/in/randson-alves/">
+                            <img src={randson} alt="randson" className='creator' />
+                        </a>
+                        <a href="https://www.instagram.com/dimitryyalves/">
+                            <img src={dimitry} alt="dimitry" className='creator' />
+                        </a>
+
                     </div>
                 </div>
             </div>
